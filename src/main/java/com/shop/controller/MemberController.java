cc8893b81fb75a263720d6e76422ab51fef1624c
GitST50 <tlstjdxo50@gmail.com>
2023-05-21 17:09:07 +0900

    first commit

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+@RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
