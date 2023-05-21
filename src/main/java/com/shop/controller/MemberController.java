d214368297cb85f0249b369c0fc224dfd8e1bd0e
GitST50 <tlstjdxo50@gmail.com>
2023-05-21 18:05:16 +0900

    first commit

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -8,9 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -31,6 +34,24 @@ public class MemberController {
 
         Member member = Member.createMember(memberFormDto, passwordEncoder);
         memberService.saveMember(member);
+
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/new")  // @Valid:객체안으로 들어오는 값에 대해 검증어노테이션 사용가능
+    public String newMember(@Valid MemberFormDto memberFormDto,
+                            BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){ //검사후 결과는 bindingResult 파라미터에 담아줌
+            return "member/memberForm";  //bindingResult.hasError를 호출하여 에러가있다면 회원가입페이지로 이동
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage()); //중복회원가입 에러 발생시 에러메시지 뷰로 전달
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
