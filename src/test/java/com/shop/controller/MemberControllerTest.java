1d7b28c6e55fd210e3b4cde71c2579bfe60498d3
GitST50 <tlstjdxo50@gmail.com>
2023-05-21 22:45:14 +0900

    first commit

---
+++ src/test/java/com/shop/controller/MemberControllerTest.java
@@ -0,0 +1,56 @@
+package com.shop.controller;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import com.shop.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc  //MockMvc 테스트를 위한 어노테이션
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class MemberControllerTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;  //테스트에 필요한 기능만가지는 가짜 객체(웹브라우저에서 요청하는것처럼 테스트)
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword(password);
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception{
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email")   //userParameter()로 이메일을 아이디로 셋팅후 로그인 URL에 요청
+                .loginProcessingUrl("/members/login")  //회원가입후 가입된회원정보로 로그인되는지 테스트
+                .user(email).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated()); //로그인 성공/인증 후 테스트코드 통과
+    }
+
+}
