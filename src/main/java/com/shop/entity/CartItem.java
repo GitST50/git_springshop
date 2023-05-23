5143a86f8227570c0f8f2e5ab9c483b0fcfd1434
GitST50 <tlstjdxo50@gmail.com>
2023-05-24 01:54:54 +0900

    first commit

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,30 @@
+package com.shop.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;   //다대일로 1개의 Cart에 여러개의 Cart_item 을 맵핑
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;   //다대일로 1개의 item을 여러개의 장바구니에 담길수있도록 item_id를 매핑
+
+    private int count; //물품의 갯수
+
+}
