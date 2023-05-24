3a24631b4b02f9570c19d6ba7881195d9d97c0dd
GitST50 <tlstjdxo50@gmail.com>
2023-05-25 01:59:14 +0900

    first commit

--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -3,6 +3,7 @@ package com.shop.entity;
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,9 @@ public class OrderTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -103,6 +107,18 @@ public class OrderTest {
         Order order = this.createOrder();
         order.getOrderItems().remove(0); //order 엔티티에서 관리하는 orderitem List의 0번째 index를 제거
         em.flush();
+    }
+
+    @Test
+    @DisplayName("지연 로딩 테스트")  //현재는 OrderItem클래스에서 즉시로딩방식으로 설정되어있음:쿼리문을 전부 불러옴
+    public void lazyLoadingTest(){
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        System.out.println("Order class : " + orderItem.getOrder().getClass());
     }
 
 
