a1dcc89664cdb01323f0176675960aec9e441dbf
GitST50 <tlstjdxo50@gmail.com>
2023-05-24 22:57:09 +0900

    first commit

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -29,7 +29,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; //주문상태
 
-    @OneToMany(mappedBy = "order") //연관관계의 주인의 필드인 order 를 설정 (OrderItem 에 존재)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) //연관관계의 주인의 필드인 order 를 설정 (OrderItem 에 존재), 영속성상태변화 자식엔티티에 모두전이:ALL
     private List<OrderItem> orderItems = new ArrayList<>(); //하나의 주문이 여러개의 주문상품을 가짐(OrderItem 쪽이 다 이기때문에 list 로 매핑)
 
     private LocalDateTime regTime;
