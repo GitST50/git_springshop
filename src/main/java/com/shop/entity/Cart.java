2604155d4572ee5f40b4bf74599b8fb0ca0e5b23
GitST50 <tlstjdxo50@gmail.com>
2023-05-25 17:37:17 +0900

    first commit

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -19,7 +19,7 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne  //일대일 맵핑
+    @OneToOne(fetch = FetchType.LAZY)  //일대일 맵핑
     @JoinColumn(name = "member_id") //매핑할 외래키를 지정 (name에 member_id(외래키)지정)
     private Member member;
 }
--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -17,11 +17,11 @@ public class CartItem {
     @Column(name = "cart_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cart_id")
     private Cart cart;   //다대일로 1개의 Cart에 여러개의 Cart_item 을 맵핑
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;   //다대일로 1개의 item을 여러개의 장바구니에 담길수있도록 item_id를 매핑
 
