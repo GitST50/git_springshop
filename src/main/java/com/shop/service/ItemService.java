911e69f82741a22d124e7cfa6f2e46edfe2ce527
GitST50 <tlstjdxo50@gmail.com>
2023-05-28 03:04:09 +0900

    first commit

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -45,5 +48,22 @@ public class ItemService {
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i)); //상품의 이미지정보를 저장
         }
         return item.getId();
+    }
+
+    @Transactional(readOnly = true) //상품데이터를 읽어오는 트랜잭션을 읽기전용으로 설정: JPA가 변경감지를 수행하지않아 성능향상
+    public ItemFormDto getItemDtl(Long itemId){
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId); //해당상품이미지 조회, 오름차순
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for(ItemImg itemImg : itemImgList){  //조회한 ItemImg 를 ItemImgDto 로 만들어서 List에 추가
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new); //상품의 아이디를 통해 상품 엔티티를 조회 존재하지않으면 Exception
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+
+        return itemFormDto;
     }
 }
