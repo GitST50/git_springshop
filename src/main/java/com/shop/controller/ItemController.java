1365e53dd2b4d71b75127f713eaec927a07b7b8d
GitST50 <tlstjdxo50@gmail.com>
2023-05-28 01:03:09 +0900

    first commit

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,12 +1,24 @@
 package com.shop.controller;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
 
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
@@ -14,5 +26,15 @@ public class ItemController {
         model.addAttribute("itemFormDto", new ItemFormDto());
 
         return "/item/itemForm";
+    }
+
+    @PostMapping(value = "/admin/item/new")
+    public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model,
+                          @RequestParam("itemImgFile")List<MultipartFile> itemImgFileList)
+    {
+        if(bindingResult.hasErrors())
+        {
+
+        }
     }
 }
