e9db3d41b7e3757ae7308b9da3eea7086873d686
GitST50 <tlstjdxo50@gmail.com>
2023-05-31 22:24:46 +0900

    first commit

--- src/main/java/com/shop/dto/MainItemDto.java
+++ src/main/java/com/shop/dto/MainItemDto.java
@@ -18,10 +18,15 @@ public class MainItemDto {
 
     private Integer price;
 
-    @QueryProjection
+    @QueryProjection //생성자에 어노테이션 선언하여 Querydsl로 조회시 MainItemDto객체로 바로 받아옴
     public MainItemDto(Long id, String itemNm, String itemDetail, String imgUrl, Integer price)
     {
 
+        this.id=id;
+        this.itemNm=itemNm;
+        this.itemDetail=itemDetail;
+        this.imgUrl=imgUrl;
+        this.price=price;
     }
 
 }
--- src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.shop.repository;
 
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,5 +10,7 @@ public interface ItemRepositoryCustom {
 
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable); //상품 조회 조건을 담고있는 itemSearchDto와 페이징정보를 담은 Pageable을 파라미터로 받음
                                                                                  //Page<item> 을 리턴
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto,Pageable pageable);
 
 }
