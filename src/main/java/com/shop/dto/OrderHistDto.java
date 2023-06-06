32e45b2264e6a84c3c385f2f88c6e024d8750f00
GitST50 <tlstjdxo50@gmail.com>
2023-06-07 02:50:07 +0900

    first commit

--- src/main/java/com/shop/dto/OrderHistDto.java
+++ src/main/java/com/shop/dto/OrderHistDto.java
@@ -1,9 +1,11 @@
 package com.shop.dto;
 
 import com.shop.constant.OrderStatus;
+import com.shop.entity.Order;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +21,15 @@ public class OrderHistDto { //주문정보
 
     private List<OrderItemDto> orderItemDtoList = new ArrayList<>();
 
-    public OrderHistDto(){
+    public OrderHistDto(Order order){  //order객체를 파라미터로 받아 멤버변수값 세팅, 주문날짜 포맷 아래와같이 설정
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = order.getOrderStatus();
 
+    }
+
+    public void addOrderItemDto(OrderItemDto orderItemDto){  //orderItemDto 객체를 주문상품 리스트에 추가
+        orderItemDtoList.add(orderItemDto);
     }
 
 }
